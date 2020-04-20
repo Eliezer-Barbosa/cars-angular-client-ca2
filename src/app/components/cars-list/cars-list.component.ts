38131bd4d32fd2f7fa172f7be295d20eb378ecde
Eliezer <eliezer1789j@gmail.com>
2020-04-20 16:26:02 +0100

    implementing the car list component

--- src/app/components/cars-list/cars-list.component.ts
+++ src/app/components/cars-list/cars-list.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { CarService } from 'src/app/services/car.service';
 
 @Component({
   selector: 'app-cars-list',
   templateUrl: './cars-list.component.html',
-  styleUrls: ['./cars-list.component.css']
+  styleUrls: ['./cars-list.component.css'],
 })
 export class CarsListComponent implements OnInit {
 
-  constructor() { }
+  public cars: any;
+  public currentCar = null;
+  public currentIndex = -1;
+  public name = '';
 
-  ngOnInit() {
+  constructor(private carService: CarService) { }
+
+  public ngOnInit() {
+    this.retrieveCars();
   }
 
+  public retrieveCars() {
+    this.carService.getAll()
+      .subscribe(
+        (data) => {
+          this.cars = data;
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+        });
+  }
+
+  public refreshList() {
+    this.retrieveCars();
+    this.currentCar = null;
+    this.currentIndex = -1;
+  }
+
+  public setActiveCar(car, index) {
+    this.currentCar = car;
+    this.currentIndex = index;
+  }
+
+  public removeAllCars() {
+    this.carService.deleteAll()
+      .subscribe(
+        (response) => {
+          console.log(response);
+          this.retrieveCars();
+        },
+        (error) => {
+          console.log(error);
+        });
+  }
+
+  public searchName() {
+    this.carService.findByName(this.name)
+      .subscribe(
+        (data) => {
+          this.cars = data;
+          console.log(data);
+        },
+        (error) => {
+          console.log(error);
+        });
+  }
 }
