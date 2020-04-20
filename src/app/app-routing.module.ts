8d8673caf003f9bf45627b7a26180c9f69172767
Eliezer <eliezer1789j@gmail.com>
2020-04-20 15:10:26 +0100

    adding car components to the router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AddCarComponent } from './components/add-car/add-car.component';
+import { CarDetailsComponent } from './components/car-details/car-details.component';
+import { CarsListComponent } from './components/cars-list/cars-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'cars', pathMatch: 'full' },
+  { path: 'cars', component: CarsListComponent },
+  { path: 'cars/:id', component: CarDetailsComponent },
+  { path: 'add', component: AddCarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
