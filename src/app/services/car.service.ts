6850df12e0d0b4f2bd88dcc1cb56e1131da20548
Eliezer <eliezer1789j@gmail.com>
2020-04-20 16:06:09 +0100

    adding service methods in car service

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+const baseUrl = 'http://localhost:8080/api/cars';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CarService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getAll() {
+    return this.http.get(baseUrl);
+  }
+
+  public get(id: any) {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  public create(data: any) {
+    return this.http.post(baseUrl, data);
+  }
+
+  public update(id: any, data: any) {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  public delete(id: any) {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  public deleteAll() {
+    return this.http.delete(baseUrl);
+  }
+
+  public findByName(name: any) {
+    return this.http.get(`${baseUrl}?name=${name}`);
+  }
 }
