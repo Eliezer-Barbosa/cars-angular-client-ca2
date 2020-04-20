9e2f5ba545587ebc1c0f38bd931653217aa368a5
Eliezer <eliezer1789j@gmail.com>
2020-04-20 15:04:18 +0100

    adding forms and http modules in the app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddCarComponent } from './components/add-car/add-car.component';
@@ -16,7 +18,9 @@ import { CarsListComponent } from './components/cars-list/cars-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
