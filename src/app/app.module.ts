9e9b036d74b3dab9dbb897dc8f169ce674386959
Eliezer <eliezer1789j@gmail.com>
2020-04-20 15:01:00 +0100

    using angular to generate car service and app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCarComponent } from './components/add-car/add-car.component';
+import { CarDetailsComponent } from './components/car-details/car-details.component';
+import { CarsListComponent } from './components/cars-list/cars-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCarComponent,
+    CarDetailsComponent,
+    CarsListComponent
   ],
   imports: [
     BrowserModule,
