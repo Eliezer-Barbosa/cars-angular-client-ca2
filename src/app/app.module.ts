3e28162e614d64ec865b325d169cc97f51118680
Eliezer <eliezer1789j@gmail.com>
2020-04-24 10:22:43 +0100

    using slider component to get car year

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
 import { MessageService } from 'primeng/api';
 import { ConfirmationService } from 'primeng/api';
+import { CardModule } from 'primeng/card';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { DropdownModule } from 'primeng/dropdown';
 import { KeyFilterModule } from 'primeng/keyfilter';
 import { MessageModule } from 'primeng/message';
 import { MessagesModule } from 'primeng/messages';
 import { PaginatorModule } from 'primeng/paginator';
+import { SliderModule } from 'primeng/slider';
 import { TableModule } from 'primeng/table';
 import { ToastModule } from 'primeng/toast';
 import { TooltipModule } from 'primeng/tooltip';
+
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -45,6 +48,8 @@ import { CarsListComponent } from './components/cars-list/cars-list.component';
     TableModule,
     TooltipModule,
     ConfirmDialogModule,
+    CardModule,
+    SliderModule,
   ],
   providers: [MessageService, ConfirmationService],
   bootstrap: [AppComponent],
